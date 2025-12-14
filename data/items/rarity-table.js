7443bfc2f2b68eabb0a7239718939105e89d74f0
sakuraebiage <hanahana1437.29@gmail.com>
2025-12-15 00:01:11 +0900

    Create rarity-table.js

---
+++ data/items/rarity-table.js
@@ -0,0 +1,16 @@
+// data/items/rarity-table.js
+
+export const RARITY = {
+  common: {
+    label: "一般",
+    color: "#cccccc"
+  },
+  uncommon: {
+    label: "希少",
+    color: "#6be0ff"
+  },
+  rare: {
+    label: "レア",
+    color: "#ff7bff"
+  }
+};
