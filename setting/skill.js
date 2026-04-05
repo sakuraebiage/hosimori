ec0a9b12bbe39ecd43d1b38886832fbe8b42898e
sakuraebiage <hanahana1437.29@gmail.com>
2026-04-05 13:44:56 +0900

    skill.js を更新

--- setting/skill.js
+++ setting/skill.js
@@ -1,30 +1,3 @@
-// skills.js
-const Skills = {
-  "attack": {
-    name: "攻撃",
-    type: "physical",
-    power: 100,
-    cost: { mp: 0, sp: 0 },
-    target: "enemy",
-    animation: "slash",
-    message: (user, target) => `${user}の攻撃！→${target}にダメージ！`
-  },
-  "heal": {
-    name: "ヒール",
-    type: "magic",
-    power: -80, // マイナスは回復
-    cost: { mp: 10, sp: 0 },
-    target: "ally",
-    animation: "heal",
-    message: (user, target) => `${user}は${target}を回復した！`
-  },
-  "boost": {
-    name: "ブースト",
-    type: "buff",
-    power: 0,
-    cost: { mp: 0, sp: 10 },
-    target: "ally",
-    animation: "buff",
-    message: (user, target) => `${user}は${target}の攻撃力を上げた！`
-  }
-};
+skillMaster
+learnSkill()
+checkRequirement()
