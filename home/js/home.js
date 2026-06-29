c1b5bfbb2cc841bb0e81e2dda84d87387a08fb39
sakuraebiage <hanahana1437.29@gmail.com>
2026-06-29 09:21:34 +0900

    home.js を更新

--- home/js/home.js
+++ home/js/home.js
@@ -1,1 +1,9 @@
 alert("読み込まれた！！");
+const backgrounds = [
+    "images/backgrounds/yozuki_day.png"
+];
+
+console.log("背景ランダム成功");
+
+document.getElementById("backgroundImage").src =
+backgrounds[0];
