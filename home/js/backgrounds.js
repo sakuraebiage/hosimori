071cf7d92f5fdac30a36055b1005b293140d8796
sakuraebiage <hanahana1437.29@gmail.com>
2026-07-02 13:23:08 +0900

    backgrounds.js を更新

---
+++ home/js/backgrounds.js
@@ -0,0 +1,54 @@
+/*==================================================
+星守戦線
+
+Background Master
+
+ホーム画面背景一覧
+
+==================================================*/
+
+const BACKGROUNDS = {
+
+    yougetsu:{
+
+        id:"yougetsu",
+
+        name:"煌萊都市・耀月",
+
+        image:"images/backgrounds/yougetsu_day.webp",
+
+        category:"city",
+
+        unlock:true
+
+    },
+
+    halfilia:{
+
+        id:"halfilia",
+
+        name:"ハルフィリア湖",
+
+        image:"images/backgrounds/halfilia_day.webp",
+
+        category:"nature",
+
+        unlock:true
+
+    },
+
+    kvaris:{
+
+        id:"kvaris",
+
+        name:"クヴァリス",
+
+        image:"images/backgrounds/kvaris.webp",
+
+        category:"snow",
+
+        unlock:true
+
+    }
+
+};
