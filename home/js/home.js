41c96f520edd65e3e027fd2617da20993b31393d
sakuraebiage <hanahana1437.29@gmail.com>
2026-07-02 13:23:20 +0900

    home.js を更新

--- home/js/home.js
+++ home/js/home.js
@@ -21,3 +21,119 @@ header.addEventListener("click",()=>{
     terminal.classList.toggle("open");
 
 });
+
+/*==================================================
+Home
+
+Background Controller
+
+==================================================*/
+
+const backgroundLayer =
+    document.getElementById("backgroundLayer");
+
+
+/*==================================================
+背景変更
+
+==================================================*/
+
+function setBackground(id){
+
+    const bg = BACKGROUNDS[id];
+
+    if(!bg){
+
+        console.warn("背景が存在しません :",id);
+
+        return;
+
+    }
+
+    backgroundLayer.style.backgroundImage =
+        `url("${bg.image}")`;
+
+}
+
+
+/*==================================================
+保存
+
+==================================================*/
+
+function saveBackground(id){
+
+    localStorage.setItem(
+
+        "homeBackground",
+
+        id
+
+    );
+
+}
+
+
+/*==================================================
+読込
+
+==================================================*/
+
+function loadBackground(){
+
+    const id = localStorage.getItem(
+
+        "homeBackground"
+
+    );
+
+    if(
+
+        id &&
+        BACKGROUNDS[id]
+
+    ){
+
+        setBackground(id);
+
+    }
+
+    else{
+
+        setBackground("yougetsu");
+
+    }
+
+}
+
+
+/*==================================================
+設定変更
+
+==================================================*/
+
+function changeBackground(id){
+
+    setBackground(id);
+
+    saveBackground(id);
+
+}
+
+
+/*==================================================
+初期化
+
+==================================================*/
+
+window.addEventListener(
+
+    "DOMContentLoaded",
+
+    ()=>{
+
+        loadBackground();
+
+    }
+
+);
